07cd90f5a4db2049be4daf0942f7d668d9739424
Nathan Thury <nathan95230@gmail.com>
2022-05-08 15:51:29 +0200

    Read data from GCS

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from storage_utils import read_data_from_gcs
+
+read_data_from_gcs("","")
--- src/storage_utils.py
+++ src/storage_utils.py
@@ -1,3 +1,9 @@
+from google.cloud import storage
+
+client = storage.Client()
+
 def read_data_from_gcs(date, tweet_keyword):
-    None
+    bucket = client.get_bucket('tweets-project-esme')
+    blob = bucket.get_blob("/tweets/2022-05-08/macron/tweets.json")
+    print(blob)
 
