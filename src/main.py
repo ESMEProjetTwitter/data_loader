e7db8b965abb69c83fcdeb9f77466ad74a4041ed
Nathan Thury <nathan95230@gmail.com>
2022-05-12 17:27:21 +0200

    small changes on storage

--- src/main.py
+++ src/main.py
@@ -1,3 +1,3 @@
 from storage_utils import read_data_from_gcs
 
-read_data_from_gcs("","")
+read_data_from_gcs("2022-05-08", "macron")
--- src/storage_utils.py
+++ src/storage_utils.py
@@ -1,9 +1,11 @@
 from google.cloud import storage
+import json
 
 client = storage.Client()
 
 def read_data_from_gcs(date, tweet_keyword):
     bucket = client.get_bucket('tweets-project-esme')
-    blob = bucket.get_blob("/tweets/2022-05-08/macron/tweets.json")
-    print(blob)
-
+    blob = bucket.get_blob("tweets/{}/{}/tweets.json".format(date, tweet_keyword))
+    data = json.loads(blob.download_as_string(client=None))
+    print(data)
+    return data
